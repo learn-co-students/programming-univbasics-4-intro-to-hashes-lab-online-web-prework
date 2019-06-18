edf1bdef0fbc9d9d85a485a2780a710a5eec455b
Patrick Harrison <jahman@bellatlantic.net>
2019-06-18 11:50:53 +0000

    Done.

---
+++ spec/hashes.rb
@@ -0,0 +1,21 @@
+def 
+  new_hash. = {}
+end
+def
+my_hash. = { dog : "Harry" }
+end
+def
+pioneer. ={:name => "Grace Hopper"}
+end
+def
+id_generator = {[:id] => 15}
+end
+def
+my_hash_creator={key:"value"}
+end
+def
+read_from_hash={name:"john",city:"name"}
+end
+def
+update_counting_hash{hash[:new key]="New Value"}
+end
