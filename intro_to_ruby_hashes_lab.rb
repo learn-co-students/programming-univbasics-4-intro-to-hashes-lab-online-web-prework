e0c6fcb6c9083180a0788dd1919aa4121a029bf5
github username <github email address>
2019-08-02 14:02:00 -0400

    Done.

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -34,15 +34,14 @@ def update_counting_hash(hash, key)
   # if the provided key is not present in the hash, add it and assign it to the value of 1
   # if the provided key is present, increment its value by 1
 
-  hash[key] ? hash[key] += 1 : hash[key] = 1;
+  # hash[key] ? hash[key] += 1 : hash[key] = 1; # this works, but below works too...
+  # return hash; # this works, but below works too...
 
-  return hash;
+  if !!hash[key]
+    hash[key] += 1; # if the provided key is present, increment its value by 1
+  else
+    hash[key] = 1; # if the provided key is not present in the hash, add it and assign it to the value of 1
+  end
 
-  # if !!hash[key]
-  #   hash[key] += 1;
-  # else
-  #   hash[key] = 1;
-  # end
-  #
-  # return hash;
+  return hash; # given a hash an a key as parameters, return an updated hash
 end
