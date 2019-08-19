d76373457502d6d06973ec2e7e22fbbcf272b4a2
Brianna Jordan <bryvjordan9@gmail.com>
2019-08-19 04:15:56 +0000

    Done.

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -1,6 +1,3 @@
-require_relative 'spec_helper'
-require_relative '../intro_to_ruby_hashes_lab.rb'
-
 def new_hash
   new_hash ={}
   # return an empty hash
@@ -21,16 +18,19 @@ def id_generator
   # return a hash with a key :id assigned to positive integer
 end
 
-def my_hash_creator(hash, key)
-
-# return a hash that includes the key and value parameters passed into this method
+def my_hash_creator(key, value)
+  my_hash_creator =   {
+    key => value
+  }
 end
 
 def read_from_hash(hash, key)
-  # return the correct value using the hash and key parameters
+  puts read_from_hash
 end
+  end
 
 def update_counting_hash(hash, key)
+
   # given a hash an a key as parameters, return an updated hash
   # if the provided key is not present in the hash, add it and assign it to the value of 1
   # if the provided key is present, increment its value by 1
