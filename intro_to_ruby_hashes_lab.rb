87648fecfc8537dbb356e4a587c684aba26f1a39
John Reyes <john.jreye@gmail.com>
2019-08-25 02:05:34 +0000

    Done.

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -1,29 +1,43 @@
-def new_hash
-  # return an empty hash
+#1
+new_hash = Hash.new
+
+#2
+my_teams = {
+  team_one: "Yankees", sport_one: "Baseball",
+  team_two: "Giants", sport_two: "Football"
+}
+  
+#3
+pioneer = { 
+  name: 'Grace Hopper'
+}
+
+#4
+id_generator = {
+  :age => 23
+}
+
+#5
+yankee_titles = {
+  :total => 27,
+  :most_recent => 2009
+}
+
+#6
+hometown = {
+  name: "Far Rock",
+  loc: "Queens"
+}
+
+hometown[:established]
+
+hometown[:established] = 1899
+
 end
 
-def my_hash
-  # return a valid hash with any key/value pair of your choice
-end
-
-def pioneer
-  # return a hash with a key of :name and a corresponding value of 'Grace Hopper'
-end
-
-def id_generator
-  # return a hash with a key :id assigned to positive integer
-end
-
-def my_hash_creator(key, value)
-  # return a hash that includes the key and value parameters passed into this method
-end
-
-def read_from_hash(hash, key)
-  # return the correct value using the hash and key parameters
-end
-
-def update_counting_hash(hash, key)
-  # given a hash an a key as parameters, return an updated hash
-  # if the provided key is not present in the hash, add it and assign it to the value of 1
-  # if the provided key is present, increment its value by 1
+#7
+if yankee_titles[:amount]
+  yankee_titles[:amount] += 1
+else
+  yankee_titles[:amount] = 27
 end
