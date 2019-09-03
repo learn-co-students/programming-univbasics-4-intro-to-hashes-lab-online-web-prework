0e55cdb54d5dc7949e3b434fd413d193444ac9a5
Kristan Johns <kristandjohns@gmail.com>
2019-09-03 23:38:41 +0000

    Done.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,9 +1,11 @@
 require 'yaml'
+rubies = YAML.load(File.read(File.join(__dir__, '..', '.travis.yml')))['rvm']
+  latest_mri = rubies.select { |v| v =~ /\A\d+\.\d+.\d+\z/ }.max
 
 RSpec.configure do |config|
-
+ config.expect_with :rspec do |expectations|
   def __
-    # raise "replace underscore with code"
+    config.mock_with :rspec do |mocks|
   end
 
 end
