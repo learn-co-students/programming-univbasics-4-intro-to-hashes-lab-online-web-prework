5ace743b2367ee291d46072e4206731eb5e070b8
Joseph Miller <jdeshaiseworldtv@gmail.com>
2019-09-22 01:48:57 +0000

    Done.

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -1,21 +1,56 @@
 def new_hash
-  # return an empty hash
+  
+      {
+        fallout => 1
+   call of duty => 2        
+     crackdown  => 3
+
+
+      } 
+  
 end
 
 def my_hash
-  # return a valid hash with any key/value pair of your choice
+  
+  {
+    
+  crackdown => 3
+  sea of thefts =>4
+  
+  }
+  
 end
 
 def pioneer
   # return a hash with a key of :name and a corresponding value of 'Grace Hopper'
+   {
+    store called :pioneer
+    
+    product
+    
+    Grace Hopper: 2.89
+      apples:     1.04
+      ice cream:  2.50
+      
+    }
+  
 end
 
 def id_generator
   # return a hash with a key :id assigned to positive integer
+  
+  id = (9+9)
+  id = (9+9)
+  
 end
 
 def my_hash_creator(key, value)
-  # return a hash that includes the key and value parameters passed into this method
+  # return that includes the key and value parameters passed into this method
+  
+$ irb
+ abc.my_hash_creator => -4040993326219951627
+  
+  
 end
 
 def read_from_hash(hash, key)
@@ -26,4 +61,16 @@ def update_counting_hash(hash, key)
   # given a hash an a key as parameters, return an updated hash
   # if the provided key is not present in the hash, add it and assign it to the value of 1
   # if the provided key is present, increment its value by 1
+  
+  {
+    :popcorn => 4
+    
+  }
+    :Soda   =>  5
+  {
+    :popcorn => 4
+    :Soda    => 1
+  }
+  
+   
 end
