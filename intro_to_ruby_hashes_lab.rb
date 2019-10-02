7a966b4ab6a9d17b9da378330390e75538a78d87
Hasan Khan <hassankhankabacha@gmail.com>
2019-10-02 20:28:38 +0000

    Done.

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -1,9 +1,10 @@
 def new_hash
-  # return an empty hash
+  new_hash = hash.new
+  #=> {}
 end
 
 def my_hash
-  # return a valid hash with any key/value pair of your choice
+  # my_hash = ["milk", "eggs", "bread"]
 end
 
 def pioneer
