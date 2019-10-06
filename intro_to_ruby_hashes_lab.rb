07535cd9537f7a7a9b1a4e10cca006f7946d67fc
William Pannell <lpann5@gmail.com>
2019-10-06 14:59:01 +0000

    Done.

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -32,9 +32,6 @@ def my_hash_creator(key, value)
 end
 
 def read_from_hash(hash, key)
-  hash = {
-    key => "Steve"
-  }
   return hash[key]
 end
 
@@ -42,4 +39,10 @@ def update_counting_hash(hash, key)
   # given a hash an a key as parameters, return an updated hash
   # if the provided key is not present in the hash, add it and assign it to the value of 1
   # if the provided key is present, increment its value by 1
+  if hash[key]
+  hash[key] += 1
+  else
+  hash[key] = 1
+  end
+  return hash
 end
