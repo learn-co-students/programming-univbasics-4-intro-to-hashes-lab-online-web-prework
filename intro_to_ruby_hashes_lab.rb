cce2ff972c552069a3edd792c34342ac29e02a3c
Sean Good <tree662@protonmail.com>
2019-10-26 23:42:42 +0000

    Done.

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -3,16 +3,11 @@ def new_hash
 end
 
 def my_hash 
-  {
-  :name => "Sean",
-  :age => "39"
-  }
+  {:name => "Sean", :age => "39"}
 end
 
 def pioneer
-  {
-    :name => "Grace Hopper"
-  }
+  {:name => "Grace Hopper"}
 end
 
 def id_generator
@@ -24,11 +19,14 @@ def my_hash_creator(key, value)
 end
 
 def read_from_hash(hash, key)
-  hash["key"]
+  hash[key]
 end
 
-# def update_counting_hash(hash, key)
-#   # given a hash an a key as parameters, return an updated hash
-#   # if the provided key is not present in the hash, add it and assign it to the value of 1
-#   # if the provided key is present, increment its value by 1
-# end
+def update_counting_hash(hash, key)
+  if hash[key]
+    hash[key] += 1 
+  else
+    hash[key] = 1
+  end
+ return hash
+end
