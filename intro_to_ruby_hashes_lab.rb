3f749f3eb95226b2df227596714b821ff3d5ebf0
Adeola Odeyemi <dbrizomomi@icloud.com>
2019-11-29 06:31:32 +0000

    Done.

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -1,9 +1,11 @@
 def new_hash
-  # return an empty hash
+  :new_hash => {}
 end
 
 def my_hash
-  # return a valid hash with any key/value pair of your choice
+ :my_hash => {
+   :cup => {(name = "Ade", age = "25")
+ }
 end
 
 def pioneer
