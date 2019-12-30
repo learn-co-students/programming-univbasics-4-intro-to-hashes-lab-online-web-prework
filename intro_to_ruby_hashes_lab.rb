b1266cd8f174210ed974cb47f874f0899502f90f
Alehandro Persaud <andrew041994@gmail.com>
2019-12-30 17:52:50 +0000

    Done.

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -1,26 +1,23 @@
+
 def new_hash
-    new_hash=
     {
 
     }
 end
 
 def my_hash
-  my_hash=
   {
     name: "Andrew"
   }
 end
 
 def pioneer
-    pioneer=
     {
       name: "Grace Hopper"
     }
 end
 
 def id_generator
-  id_generator =
   {
     id: 5
   }
@@ -36,11 +33,11 @@ def read_from_hash(hash, key)
 end
 
 def update_counting_hash(hash, key)
-  # given a hash an a key as parameters, return an updated hash
-  # if the provided key is not present in the hash, add it and assign it to the value of 1
-  # if the provided key is present, increment its value by 1
 
-    hash[key] ? hash[key] += 1 : hash[key] = 1
-    hash
-
+  if hash[key]
+     hash[key]= hash[key] +1
+  else
+    hash[key]=1
+  end
+  hash
 end
